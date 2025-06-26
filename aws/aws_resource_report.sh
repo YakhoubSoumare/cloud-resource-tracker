6958a1f565159a660ac9009899c41db94c522d77
YakhoubSoumare <yakhoub.soumare1990@gmail.com>
2025-06-26 15:38:01 +0000

    Add commands to list S3 buckets, Lambda functions, and IAM users

--- aws/aws_resource_report.sh
+++ aws/aws_resource_report.sh
@@ -15,3 +15,15 @@ set -o		# Cancel on pipefailure
 # List all instances ids
 echo "===== EC2 Instances ====="
 aws ec2 describe-instances | jq '.Reservations[].Instances[].InstanceId'
+
+# List all S3 buckets
+echo "===== S3 Buckets ====="
+aws s3 ls
+
+# List all Lambda functions
+echo "===== Lambda Functions ====="
+aws lambda list-functions
+
+# List all IAM users
+echo "===== IAM Users ====="
+aws iam list-users
